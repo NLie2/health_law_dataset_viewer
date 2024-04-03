e3af807a2f94054f39f0ea4dc1fddb7b5d539bc9
Nathalie <nathalie.kirch.nk@gmail.com>
2024-04-03 23:03:28 +0200

    create simple dataset viewer

---
+++ dataset_viewer/src/App.jsx
@@ -0,0 +1,56 @@
+
+import './App.css'
+import question_dataset from '../../assets/question_dataset.json';
+
+import { useState } from 'react';
+
+function App() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const nextQuestion = () => {
+    if (questionIndex < question_dataset.length - 1) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      setQuestionIndex(0);
+    }
+  }
+  console.log(question_dataset[questionIndex])
+  const previousQuestion = () => {
+    if (questionIndex > 0) {
+      setQuestionIndex(questionIndex - 1);
+    } else{
+      setQuestionIndex(question_dataset.length - 1);
+    }
+
+  }
+  const handleClick = (e) => {
+    e.target.id === "previous" ? previousQuestion() : nextQuestion();
+  }
+  return (
+    <>
+      <main>
+        <div className="button-div">
+          <button className="control-button" id="previous" onClick={handleClick}>previous</button>
+          <button className="control-button" id="next" onClick={handleClick}>Next</button>
+        </div>
+        <h1>Dataset Viewer {questionIndex}</h1>
+        <div className="source">
+            <p>{question_dataset[questionIndex]["source_text"]}</p>
+          </div>
+        <div className="card">
+          <div className="questionAnswer">
+            <h2>Question</h2>
+            <p>{question_dataset[questionIndex]["questions_gpt-4"]}</p>
+            <h3>{question_dataset[questionIndex]["answer_options"].split('.')[0]}</h3>
+            <h3>{question_dataset[questionIndex]["answer_options"].split('.')[1]}</h3>
+          </div>
+          <div className="questionAnswer">
+            <h2>Answer: {question_dataset[questionIndex]["gold_answers_gpt-4"]}</h2>
+            <p>{question_dataset[questionIndex]["gold_reasoning_gpt-4"]}</p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
