740749b218c27a693cc8961f4aa8bf4fe2ea69cd
Nathalie <nathalie.kirch.nk@gmail.com>
2024-04-03 23:41:42 +0200

    change base url in two places

--- dataset_viewer/vite.config.js
+++ dataset_viewer/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://<NLie2>.github.io/<health_law_dataset_viewer>"
+  base: "https://NLie2.github.io/health_law_dataset_viewer/"
 })
